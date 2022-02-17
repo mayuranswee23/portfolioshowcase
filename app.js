b6808922427f299db6a5a0320c151a00aa1b1cd4
Mayuran <mayuranswee23@gmail.com>
2022-02-17 00:45:29 -0500

    added page template and inquirer validation

--- app.js
+++ app.js
@@ -1,16 +1,8 @@
 const inquirer = require('inquirer')
-// const fs = require('fs');
-// const generatePage = require ('./src/page-template')
-
-// const pageHTML = generatePage(names, github);
+const fs = require('fs');
+const generatePage = require ('./src/page-template')
 
 
-
-// fs.writeFile('index.html', pageHTML, err => {
-//   if (err) throw err;
-
-//   console.log('Portfolio complete! Check out index.html to see the output!');
-// });
 
 const promptUser = () => {
  return inquirer.prompt([
@@ -100,5 +92,11 @@ const promptUser = () => {
   promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    console.log(portfolioData);
+    
+    const pageHTML = generatePage(portfolioData);
+
+  fs.writeFile('index.html', pageHTML, err => {
+  if (err) throw new Error (err);
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});;
   });
