ce2646034483e86283dd4ebddcb2db0aaa8fa41b
Mayuran <mayuranswee23@gmail.com>
2022-02-22 23:50:22 -0500

    final touches

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const inquirer = require('inquirer')
-const fs = require('fs');
+const { writeFile, copyFile } = require('./utils/generate-page');
 const generatePage = require ('./src/page-template')
+
+
 
 
 
@@ -89,14 +91,41 @@ const promptUser = () => {
     });
   };
 
-  promptUser()
+//   promptUser()
+//   .then(promptProject)
+//   .then(portfolioData => {
+    
+//     const pageHTML = generatePage(portfolioData);
+
+//   fs.writeFile('./dist/index.html', pageHTML, err => {
+//   if (err) throw new Error (err);
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+
+//   fs.copyFile('./src/style.css', './dist/style.css', err => {
+//     if (err) {
+//       console.log(err);
+//       return;
+//     }
+//     console.log('Style sheet copied successfully!');
+//   });
+// });
+// });;
+
+promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    
-    const pageHTML = generatePage(portfolioData);
-
-  fs.writeFile('index.html', pageHTML, err => {
-  if (err) throw new Error (err);
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});;
+    return generatePage(portfolioData);
+  })
+  .then(pageHTML => {
+    return writeFile(pageHTML);
+  })
+  .then(writeFileResponse => {
+    console.log(writeFileResponse);
+    return copyFile();
+  })
+  .then(copyFileResponse => {
+    console.log(copyFileResponse);
+  })
+  .catch(err => {
+    console.log(err);
   });
